17092f02614bc94cf46b05890988d6beb35766c9
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-03-30 23:28:30 +0545

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["dotenv", "crypto"], // Exclude these packages from being bundled
+    exclude: ["dotenv", "crypto"],
   },
   resolve: {
-    alias: {
-      // Optionally, you can also alias 'crypto' to 'crypto-browserify' if needed for the browser.
-      // 'crypto': require.resolve('crypto-browserify'),
+    alias: {},
+  },
+  build: {
+    rollupOptions: {
+      external: ["lucide-react"],
     },
   },
 });
