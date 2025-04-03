79a42f2af98c5a85c4623bb0d6b848aca867af51
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-04-03 12:39:11 +0545

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ["lucide-react", "stripe", "mongoose"],
+      external: ["lucide-react", "stripe"],
     },
   },
 });
