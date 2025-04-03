30514c8d9745578d283adebc4080808fb41950a9
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-04-03 13:10:25 +0545

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["dotenv", "crypto"],
-  },
-  resolve: {
-    alias: {},
-  },
-  build: {
-    rollupOptions: {
-      external: ["lucide-react", "stripe", "express", "mongoose"],
-    },
-  },
 });
