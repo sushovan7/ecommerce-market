0f8c168f510a9496cb1c540b00baf3216be0c5f4
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-04-03 12:50:08 +0545

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,10 +12,14 @@ import { cartRouter } from "./routes/cart.routes.js";
 import { orderRouter } from "./routes/order.routes.js";
 
 const app = express();
-
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "https://ecommercemarketadmin.vercel.app", // admin URL
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
