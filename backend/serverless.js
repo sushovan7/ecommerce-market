93e1179c5ce14c542a62443d369af7b3432da799
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-04-06 23:43:44 +0545

    update

--- backend/serverless.js
+++
@@ -1,7 +0,0 @@
-import app from "./index.js";
-import { connectDb } from "./DB/db.js";
-
-export default async function handler(req, res) {
-  await connectDb();
-  return app(req, res);
-}
--- backend/src/index.js
+++ backend/src/index.js
@@ -49,17 +49,3 @@ connectDb()
   .catch((err) => {
     console.log("mongodb connection error: ", err);
   });
-
-// export default app;
-
-// if (process.env.NODE_ENV !== "production") {
-//   connectDb()
-//     .then(() => {
-//       app.listen(process.env.PORT || 8000, () => {
-//         console.log("Server running on port:", process.env.PORT || 8000);
-//       });
-//     })
-//     .catch((err) => {
-//       console.log("MongoDB connection error:", err);
-//     });
-// }
