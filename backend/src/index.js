20edaccda156d5c822f91e3b2a5bed24178c7fe3
Ankit Bhattarai <bhattaraisushovan999@gmail.com>
2025-04-10 18:23:16 +0545

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,13 +18,7 @@ console.log(process.env);
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      process.env.FRONTEND_URL ||
-        "https://ecommercefrontend-liart-gamma.vercel.app",
-      process.env.ADMIN_URL || "https://ecommercemarketadmin.vercel.app",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
